a6883ba760c2fc1cfb11091696878bb53289bafd
AcerF <fazushkam@gmail.com>
2020-05-31 22:12:04 +0500

    yuklash

---
+++ vue-crash-course/src/components/AddTodo.vue
@@ -0,0 +1,40 @@
+<template>
+    <form @submit.prevent="onsubmit">
+        <input type="text" v-model="title">
+        <button type="submit">Create</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "AddTodo",
+        data(){
+            return{
+                title:''
+            }
+        },
+        methods:{
+            onsubmit(){
+                if (this.title.trim()){
+                    const newTodo = {
+                        id: Date.now(),
+                        title:this.title,
+                        completed: false
+                    }
+                    this.$emit('add-todo', newTodo)
+                    this.title = ''
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    form{
+        display: flex;
+    }
+    input{
+        width: 100%;
+        margin-right: 5px;
+    }
+</style>
---
+++ vue-crash-course/src/components/TodoItem.vue
@@ -0,0 +1,52 @@
+<template>
+    <li>
+        <span :class="{done: todo.completed}">
+            <input type="checkbox" @click="todo.completed = !todo.completed">
+            <strong>{{ index +1 }} </strong>
+            {{ todo.title | uppercase}}
+        </span>
+        <button class="rm"
+            @click="$emit('remove-todo',todo.id)"
+        >&times;</button>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props: {
+            todo:{
+                type:Object,
+                required: true
+            },
+            index: Number
+        },
+        filters:{
+            uppercase(value){
+                return value.toUpperCase()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    li{
+        border: 1px solid #cccccc;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 1rem;
+        padding: .5rem 2rem;
+    }
+    .rm{
+        background-color: red;
+        color:#fff;
+        border-radius: 50%;
+        font-weight: bold;
+    }
+    input{
+        margin-right: 1rem;
+    }
+    .done{
+        text-decoration: line-through;
+    }
+</style>
